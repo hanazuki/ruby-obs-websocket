49b6fb222ae552da995682da605ee88779fec266
Kasumi Hanazuki <kasumi@rollingapple.net>
2022-07-24 12:24:45 +0000

    obs-websocket v5

--- examples/websocket-driver/main.rb
+++ examples/websocket-driver/main.rb
@@ -7,23 +7,27 @@ require 'uri'
 require 'websocket/driver'
 
 class Main
-  def run(uri, password)
+  def run(uri, password = nil)
     connect(URI.parse(uri))
 
-    # To initialize OBS::WebSocket, pass a WebSocket connection.
-    obs = OBS::WebSocket::Client.new(@driver)
+    tracer = Tracer.new if ENV.key?('TRACE')
 
+    # To initialize OBS::WebSocket, pass a WebSocket connection,
+    # and optionally a tracer for debugging.
+    obs = OBS::WebSocket::Client.new(@driver, tracer: tracer)
+
+    # If the server requires authentication, set the password before starting communication.
+    obs.password = password
+
+    # Once the client is identified by the server, the handlers registered with `on_open` are called back.
     obs.on_open do
-      obs.authenticate!(password).value!
-
-      # Request methods return Future values that can be waited for by `value!`.
+      # The request methods return Future values that can be waited for by `value!`.
       version = obs.get_version.value!
-      puts "OBS version: #{version.obs_studio_version}; OBS-websocket version: #{version.obs_websocket_version}"
+      puts "OBS version: #{version.obs_version}; OBS-websocket version: #{version.obs_web_socket_version}"
 
       # Pass request parameters using keyword arguments. Composite types are mapped to Hash objects.
-      obs.broadcast_custom_message(
-        realm: 'helloworld',
-        data: {greeting: 'Hello, World!'}
+      obs.broadcast_custom_event(
+        event_data: {greeting: 'Hello, World!'}
       ).wait!
     rescue => e
       $stderr.puts e.message
@@ -31,15 +35,19 @@ class Main
     end
 
     # Listen for events using `on_*` methods. The event payload is yielded to the block.
-    obs.on_broadcast_custom_message do |ev|
+    obs.on_custom_event do |ev|
       puts ev.data['greeting']
       obs.close
     end
 
+    # When the connection is closed by the server, the handlers registered to `on_close` are called back
+    # with the error code and human-readable message.
     obs.on_close(executor: :immediate) do |code, reason|
-      puts "Bye! (code: #{code})"
+      puts "Bye! (code: #{code}; reason: #{reason})"
+      exit 1 if code != 1000
     end
 
+    # Now start the websocket driver to begin the communication with the server.
     start_driver
   end
 
@@ -49,7 +57,7 @@ class Main
     fail ArgumentError, 'Only supports ws:// URI' unless uri.scheme == 'ws'
 
     @socket = TCPSocket.new(uri.host, uri.port || 80)
-    @driver = WebSocket::Driver.client(SocketWrapper.new(uri.to_s, @socket))
+    @driver = WebSocket::Driver.client(SocketWrapper.new(uri, @socket))
   end
 
   def start_driver
@@ -64,7 +72,7 @@ class Main
 
   class SocketWrapper
     def initialize(url, socket)
-      @url = url
+      @url = url.to_s
       @socket = socket
     end
 
@@ -74,11 +82,21 @@ class Main
       @socket.write(s)
     end
   end
+
+  class Tracer
+    def send_message(msg)
+      $stderr.puts ">> #{msg}"
+    end
+
+    def receive_message(msg)
+      $stderr.puts "<< #{msg}"
+    end
+  end
 end
 
 if $0 == __FILE__
-  if ARGV.size != 2
-    $stderr.puts "Usage: #{File.basename __FILE__} ws://HOST:PORT PASSWORD"
+  unless (1..2) === ARGV.size
+    $stderr.puts "Usage: #{File.basename __FILE__} ws://HOST:PORT [PASSWORD]"
     exit 1
   end
 
